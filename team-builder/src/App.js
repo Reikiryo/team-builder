bcec928aedfce3f44fd7fcdaaeb314c1ba0eec2e
Josiah <reikiryo@yahoo.com>
2020-01-30 15:09:38 -0500

    moved bulkier code from app to container

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,57 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Container from './components/container'
+import styled from 'styled-components'
 
-const Team = [
-  { id: 0, name: "Josiah Bailey", email: "josiahbailey101@yahoo.com", role: "Leader" }
-]
 
 function App() {
-  const [team, setTeam] = useState([
-    { id: 0, name: "Josiah Bailey", email: "josiahbailey101@yahoo.com", role: "2" }
-  ])
-  const [memberToEdit, setEdit] = useState()
-
-  const editMember = member => {
-    setEdit(member)
-  }
-  const changeMember = member => {
-    const newMember = {
-      id: member.id,
-      name: member.name,
-      email: member.email,
-      role: member.role
-    }
-    const arr = [...team]
-    arr[newMember.id] = newMember
-    setTeam(arr)
-  }
-  const addMember = member => {
-    const newMember = {
-      id: team.length,
-      name: member.name,
-      email: member.email,
-      role: member.role
-    }
-    setTeam([...team, newMember])
-  }
-  const deleteMember = member => {
-    const arr = [...team]
-    arr.splice(member.id, 1)
-    arr.forEach(member => {
-      member.id = member.key
-    })
-    console.log(arr)
-    setTeam(arr)
-  }
   return (
     <div className="App">
-      <Container
-        changeMember={changeMember}
-        editMember={editMember}
-        deleteMember={deleteMember}
-        memberToEdit={memberToEdit}
-        team={team}
-        addMember={addMember} />
+      <Container />
     </div>
   );
 }
--- team-builder/src/components/container.js
+++ team-builder/src/components/container.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './forms'
 import Card from './card'
 import styled from 'styled-components'
@@ -9,7 +9,44 @@ const Cont = styled.div`
 	flex-direction: row;
 	flex-wrap: wrap;
 `
-const Container = ({ team, addMember, editMember, memberToEdit, changeMember, deleteMember }) => {
+const Container = () => {
+	const [team, setTeam] = useState([
+		{ id: 0, name: "Josiah Bailey", email: "josiahbailey101@yahoo.com", role: "2" }
+	  ])
+	  const [memberToEdit, setEdit] = useState()
+	
+	  const editMember = member => {
+		setEdit(member)
+	  }
+	  const changeMember = member => {
+		const newMember = {
+		  id: member.id,
+		  name: member.name,
+		  email: member.email,
+		  role: member.role
+		}
+		const arr = [...team]
+		arr[newMember.id] = newMember
+		setTeam(arr)
+	  }
+	  const addMember = member => {
+		const newMember = {
+		  id: team.length,
+		  name: member.name,
+		  email: member.email,
+		  role: member.role
+		}
+		setTeam([...team, newMember])
+	  }
+	  const deleteMember = member => {
+		const arr = [...team]
+		arr.splice(member.id, 1)
+		arr.forEach(member => {
+		  member.id = member.key
+		})
+		console.log(arr)
+		setTeam(arr)
+	  }
 
 	return (
 		<div>
