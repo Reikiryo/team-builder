abcbc8734459800021b23e69a3d66a54265aade2
Josiah <reikiryo@yahoo.com>
2020-01-30 00:20:10 -0500

    ads team slice of state as well as addMember function to handle form data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from './components/container'
+
+const Team = [
+  {id: 0, name: "Josiah Bailey", email: "josiahbailey101@yahoo.com", role: "Leader"}
+]
 
 function App() {
+  const [team, setTeam] = useState([
+    {id: 0, name: "Josiah Bailey", email: "josiahbailey101@yahoo.com", role: "Leader"}
+  ])
+  const addMember = member => {
+    const newMember = {
+      id: team.length,
+      name: '',
+      email: '',
+      role: ''
+    }
+    setTeam([...team, newMember])
+  }
   return (
     <div className="App">
-      
+      <Container team={team} addMember={addMember}  />
     </div>
   );
 }
---
+++ team-builder/src/components/container.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './forms'
+import Card from './card'
+
+const Container = ({ team }) => {
+	
+    return (
+		<div>
+			<Form/>
+
+		</div>
+    );
+}
+ 
+export default Container;
